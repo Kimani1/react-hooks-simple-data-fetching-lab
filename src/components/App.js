9c5f35cbe132428d2db3acbb460345451dea7973
Mark <mark.kimani1@student.moringaschool.com>
2023-09-10 10:00:04 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    // Fetch a random dog image from the API
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        // Update the state with the received dog image URL
+        setDogImage(data.message);
+      })
+      .catch((error) => {
+        console.error('Error fetching dog image:', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Random Dog Image</h1>
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
